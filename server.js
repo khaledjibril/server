90008c5ebea60c870502dee83783eb0006d8cf56
khaled J Muhammad <khaledblings@gmail.com>
2025-11-29 21:59:57 +0100

    Fix CORS origin URL by removing trailing slash

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://neriah-photography.vercel.app/"
+      "https://neriah-photography.vercel.app"
     ],
     credentials: true,
   })
